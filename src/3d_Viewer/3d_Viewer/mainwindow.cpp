3af201088fa144e6e958c167cd9c0c7450cc9259
joserans <joserans@student.21-school.ru>
2023-07-03 23:02:07 +0300

    settings pre done

--- src/3d_Viewer/3d_Viewer/mainwindow.cpp
+++ src/3d_Viewer/3d_Viewer/mainwindow.cpp
@@ -44,28 +44,60 @@ void MainWindow::set_start_information() {
 
 void MainWindow::save_settings() {
     settings->beginGroup("Common");
-    settings->setValue("background_color", ui->viewer_gl_widget->background_color);
-    settings->setValue("projection_type", ui->viewer_gl_widget->is_projection_ortho);
+        settings->setValue("background_color", ui->viewer_gl_widget->background_color);
+        settings->setValue("projection_type", ui->viewer_gl_widget->is_projection_ortho);
     settings->endGroup();
 
     settings->beginGroup("Vertex");
-    settings->setValue("vertex_type", ui->viewer_gl_widget->vert_type);
-    settings->setValue("vertex_color", ui->viewer_gl_widget->vertex_color);
-    settings->setValue("vertex_depth", ui->viewer_gl_widget->vertex_depth);
+        settings->setValue("vertex_type", ui->viewer_gl_widget->vert_type);
+        settings->setValue("vertex_color", ui->viewer_gl_widget->vertex_color);
+        settings->setValue("vertex_depth", ui->viewer_gl_widget->vertex_depth);
     settings->endGroup();
 
     settings->beginGroup("Edge");
-    settings->setValue("edge_type", ui->viewer_gl_widget->is_edge_solid);
-    settings->setValue("edge_color", ui->viewer_gl_widget->edge_color);
-    settings->setValue("edge_depth", ui->viewer_gl_widget->edge_depth);
+        settings->setValue("edge_type", ui->viewer_gl_widget->is_edge_solid);
+        settings->setValue("edge_color", ui->viewer_gl_widget->edge_color);
+        settings->setValue("edge_depth", ui->viewer_gl_widget->edge_depth);
     settings->endGroup();
 }
 
 void MainWindow::load_settings() {
     settings = new QSettings("3D", "Config");
     settings->beginGroup("Common");
+         ui->viewer_gl_widget->background_color = settings->value("background_color").toString();
+         ui->viewer_gl_widget->is_projection_ortho = settings->value("projection_type").toBool();
+    settings->endGroup();
+
+    settings->beginGroup("Vertex");
+        if (settings->value("vertex_type") == CIRLCE) {
+            ui->radio_vertex_circle->setChecked(true);
+        } else if (settings->value("vertex_type") == SQUARE) {
+            ui->radio_vertex_square->setChecked(true);
+        } else if (settings->value("vertex_type") == NOTHING) {
+            ui->radio_vertex_nothing->setChecked(true);
+        }
+
+        ui->viewer_gl_widget->vert_type = (enum vertex_type)settings->value("vertex_type").toInt();
+        ui->viewer_gl_widget->vertex_color = settings->value("vertex_color").toString();
+        ui->viewer_gl_widget->vertex_depth = settings->value("vertex_depth").toDouble();
+        ui->line_vertex_depth->setText(settings->value("vertex_depth").toString());
+    settings->endGroup();
+
+    settings->beginGroup("Edge");
+        if (settings->value("edge_type").toBool()) {
+            ui->radio_edge_solid->setChecked(true);
+        } else {
+            ui->radio_edge_dotted->setChecked(true);
+        }
+
+    ui->viewer_gl_widget->is_edge_solid = settings->value("edge_type").toBool();
+    ui->viewer_gl_widget->edge_color = settings->value("edge_color").toString();
+    ui->viewer_gl_widget->edge_depth = settings->value("edge_depth").toDouble();
+    ui->line_edge_depth->setText(settings->value("edge_depth").toString());
 
     settings->endGroup();
+
+
 }
 
 void MainWindow::on_button_pick_file_clicked()
